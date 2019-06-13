86241918231eb3f2ac7d9a3dff500a9a02961555
14sudharsan <14sudharsan@gmail.com>
2019-06-13 15:50:08 +0530

    Update getmac.sh

--- getmac.sh
+++ getmac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 read -p " Enter the MacAddress : " MacAddress
-read -p "Enter the Field Name ex vendorDetails (or) blockDetails (or) macAddressDetails (or with subfield) vendorDetails.companyName : " fieldname
+read -p " Enter Apikey : "  APIKEY
+read -p "Enter the Field Name ex vendorDetails (or with subfield) vendorDetails.companyName : " fieldname
 macdetails=$(curl -Gs "https://api.macaddress.io/v1?apiKey=$APIKEY&output=json&search=$MacAddress" | jq ".$fieldname" 
-echo $macdetails 
-#associated with that Mac Address $MacAddress
+echo $macdetails
