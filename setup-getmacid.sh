fbaca69f5d1a12286b06d756ec6e16bc605d8a19
14sudharsan <14sudharsan@gmail.com>
2019-06-13 22:03:28 +0530

    Update setup-getmacid.sh

--- setup-getmacid.sh
+++ setup-getmacid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo Downloading getmacid microservice docker image please wait ...
-docker pull 14sudharsan/getmacid:version3
+docker pull 14sudharsan/getmacid:version6
 echo Docker container spawn from container image
-docker run -it 14sudharsan/getmacid:version3
+docker run -it 14sudharsan/getmacid:version6
