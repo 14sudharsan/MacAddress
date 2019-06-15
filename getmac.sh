d937ac99dd3d261ed30d514bf857d6b9c92e4fc9
14sudharsan <14sudharsan@gmail.com>
2019-06-15 12:21:41 +0530

    Update getmac.sh

--- getmac.sh
+++ getmac.sh
@@ -25,7 +25,6 @@ If you leave blank and press enter
 esac
 #!/usr/bin/env bash
 read -p " Enter the MacAddress : " MacAddress
-read -p " Enter Apikey : "  APIKEY
 read -p "Enter the Field Name ex vendorDetails (or with subfield) vendorDetails.companyName : " fieldname
 macdetails=$(curl -Gs "https://api.macaddress.io/v1?apiKey=$APIKEY&output=json&search=$MacAddress" | jq ".$fieldname")
 echo $macdetails
