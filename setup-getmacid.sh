91df0cfcfa7bb67d25af504891d34359c29f0f97
14sudharsan <14sudharsan@gmail.com>
2019-06-15 18:11:50 +0530

    Update setup-getmacid.sh

--- setup-getmacid.sh
+++ setup-getmacid.sh
@@ -2,4 +2,4 @@
 echo Please wait for a while docker building an image from dockerfile
 docker build -t getmac .
 echo Docker container spawn from container image
-docker run -it getmac:latest /bin/sh
+docker run -it getmac:latest at_j2YdcHWY1QH2N3B2VhsLhMJXjlfC4
