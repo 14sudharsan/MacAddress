627a8d91675cd13c0b12ed75e249299a318e6611
14sudharsan <14sudharsan@gmail.com>
2019-06-15 09:14:00 +0530

    Update getmac.sh

--- getmac.sh
+++ getmac.sh
@@ -3,19 +3,20 @@ read -p "Do you need any help to get  Field Names and expected outputs (y/n)? " 
 case ${answer:0:1} in
     y|Y )
         echo "
-********************************************************************************************************************************************
-Enter the Field Name : vendorDetails
-Expected output
-{ oui: 340286, isPrivate: false, companyName: Intel Corp, companyAddress: Lot 8, Jalan Hi-Tech 2/3 Kulim Kedah 09000 MY, countryCode: MY }
-********************************************************************************************************************************************
-Enter the Field Name : vendorDetails.companyName
-Expected output
-Intel Corp
-********************************************************************************************************************************************
-Enter the Field Name : Leave Empty and press Enter
-Expected output
-It will print entire details of the respective mac address
-********************************************************************************************************************************************
+        
+       Options: for Field name
+       
+vendorDetails
+
+    * It will display vendor details to the respective mac address
+
+vendorDetails.companyName
+
+    * It will display precisely companyName subfield from vendor details use this option
+
+If you leave blank and press enter
+
+    *It will display all the details of the respective mac address
 "
     ;;
     * )
